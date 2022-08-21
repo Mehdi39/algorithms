6c6179fbca507a3d2616726059121abd54666490
Jahid <jahid4k@gmail.com>
2022-08-21 13:41:47 +0600

    TwoNumberSum modified

--- twoNumberSum.py
+++ twoNumberSum.py
@@ -1,11 +1,16 @@
-# Given an array of integers sortedArray and an integer target, 
+# Given an array of integers nums and an integer target, 
 # return indices of the two numbers such that they add up to target.
 
-def twoNumberSum(sortedArray, target):
-    for i in range(len(sortedArray) - 1):
-        firstNum = sortedArray[i]
-        for j in range(i + 1, len(sortedArray)):
-            secondNum = sortedArray[j]
+array = [3,4,5,-4,8,11,1,-1,6]
+
+# method 1
+def twoNumberSum(array, target):
+    for i in range(len(array) - 1):
+        firstNum = array[i]
+        for j in range(i + 1, len(array)):
+            secondNum = array[j]
             if firstNum + secondNum == target:
                 return [firstNum, secondNum]
     return []
+
+print(twoNumberSum(array, 10))
