1370879be406126037c220a2850513d91e104f0a
Jahid <jahid4k@gmail.com>
2023-08-05 23:42:04 +0600

    LeetCode 30

---
+++ JS/LeetCode 30 Days/AllowOneFunctionCall.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false
+    return function(...args){
+        if (called) {
+            return undefined
+        }
+        called = true
+        return fn(...args)
+    }
+};
---
+++ JS/LeetCode 30 Days/Memoize.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    let cache = {}
+    return function(...args) {
+        const key = JSON.stringify(args)
+        if (key in cache) {
+            return cache[key]
+        }
+        cache[key]=fn(...args)
+        return cache[key]
+    }
+}
